fa9e1d528ed7c2358a5ccd0f1d494820d3b861a9
crispimbarber-droid <crispimbarber@gmail.com>
2026-09-07 21:21:50 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,36 @@
-const CACHE="jean-treino-v1";
-const ASSETS=["./index.html","./manifest.webmanifest","./icons/icon-192.png","./icons/icon-512.png","./icons/icon-96.png","./icons/icon-48.png"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE = "jean-treino-v2";
+
+const ASSETS = [
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icon-48.png",
+  "./icon-96.png",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
